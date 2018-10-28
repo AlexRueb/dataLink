3629b070324d842a5533d2876d1aba0772ef5189
Dillon Tice <dtice@localhost.localdomain>
2018-10-28 17:02:28 -0600

    update sim_3

--- simulation_3.py
+++ simulation_3.py
@@ -8,14 +8,14 @@ import link_3
 import threading
 from time import sleep
 
-##configuration parameters
-router_queue_size = 0 #0 means unlimited
-simulation_time = 2 #give the network sufficient time to transfer all packets before quitting
+# configuration parameters
+router_queue_size = 0  # 0 means unlimited
+simulation_time = 2  # give the network sufficient time to transfer all packets before quitting
 
 if __name__ == '__main__':
-    object_L = [] #keeps track of objects, so we can kill their threads
+    object_L = []  # keeps track of objects, so we can kill their threads
 
-    #create network nodes
+    # create network nodes
     client_1 = network_3.Host(1)
     object_L.append(client_1)
     client_2 = network_3.Host(2)
@@ -24,13 +24,13 @@ if __name__ == '__main__':
     object_L.append(server_1)
     server_2 = network_3.Host(4)
     object_L.append(server_2)
-    router_a = network_3.Router(name='A', intf_count=1, max_queue_size=router_queue_size)
+    router_a = network_3.Router(name='A', intf_count=2, max_queue_size=router_queue_size)
     object_L.append(router_a)
     router_b = network_3.Router(name='B', intf_count=1, max_queue_size=router_queue_size)
     object_L.append(router_b)
     router_c = network_3.Router(name='C', intf_count=1, max_queue_size=router_queue_size)
     object_L.append(router_c)
-    router_d = network_3.Router(name='D', intf_count=1, max_queue_size=router_queue_size)
+    router_d = network_3.Router(name='D', intf_count=2, max_queue_size=router_queue_size)
     object_L.append(router_d)
 
     # create a Link Layer to keep track of links between network nodes
@@ -40,15 +40,22 @@ if __name__ == '__main__':
     # add all the links
     # link parameters: from_node, from_intf_num, to_node, to_intf_num, mtu
     link_layer.add_link(link_3.Link(client_1, 0, router_a, 0, 50))
+    link_layer.add_link(link_3.Link(client_1, 0, router_a, 1, 50))
     link_layer.add_link(link_3.Link(client_2, 0, router_a, 0, 50))
+    link_layer.add_link(link_3.Link(client_2, 0, router_a, 1, 50))
+
     link_layer.add_link(link_3.Link(router_a, 0, router_b, 0, 50))
+    link_layer.add_link(link_3.Link(router_a, 1, router_b, 0, 50))
     link_layer.add_link(link_3.Link(router_a, 0, router_c, 0, 50))
+    link_layer.add_link(link_3.Link(router_a, 1, router_c, 0, 50))
+
     link_layer.add_link(link_3.Link(router_b, 0, router_d, 0, 50))
     link_layer.add_link(link_3.Link(router_c, 0, router_d, 0, 50))
-    link_layer.add_link(link_3.Link(router_c, 0, router_d, 0, 50))
-    link_layer.add_link(link_3.Link(router_c, 0, router_d, 0, 50))
+
     link_layer.add_link(link_3.Link(router_d, 0, server_1, 0, 50))
+    link_layer.add_link(link_3.Link(router_d, 1, server_1, 0, 50))
     link_layer.add_link(link_3.Link(router_d, 0, server_2, 0, 50))
+    link_layer.add_link(link_3.Link(router_d, 1, server_2, 0, 50))
 
     # start all the objects
     thread_L = []
