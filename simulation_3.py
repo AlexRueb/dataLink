c2eaba6e2976e510ae00ccd04a66e645c93d68aa
Dillon Tice <dtice@localhost.localdomain>
2018-10-28 16:25:44 -0600

    sim_3

--- simulation_3.py
+++ simulation_3.py
@@ -3,8 +3,8 @@ Created on Oct 12, 2016
 
 @author: mwittie
 '''
-import network_1
-import link_1
+import network_3
+import link_3
 import threading
 from time import sleep
 
@@ -14,53 +14,72 @@ simulation_time = 2 #give the network sufficient time to transfer all packets be
 
 if __name__ == '__main__':
     object_L = [] #keeps track of objects, so we can kill their threads
-    
+
     #create network nodes
-    client = network_1.Host(1)
-    object_L.append(client)
-    server = network_1.Host(2)
-    object_L.append(server)
-    router_a = network_1.Router(name='A', intf_count=1, max_queue_size=router_queue_size)
+    client_1 = network_3.Host(1)
+    object_L.append(client_1)
+    client_2 = network_3.Host(2)
+    object_L.append(client_2)
+    server_1 = network_3.Host(3)
+    object_L.append(server_1)
+    server_2 = network_3.Host(4)
+    object_L.append(server_2)
+    router_a = network_3.Router(name='A', intf_count=1, max_queue_size=router_queue_size)
     object_L.append(router_a)
-    
-    #create a Link Layer to keep track of links between network nodes
-    link_layer = link_1.LinkLayer()
+    router_b = network_3.Router(name='B', intf_count=1, max_queue_size=router_queue_size)
+    object_L.append(router_b)
+    router_c = network_3.Router(name='C', intf_count=1, max_queue_size=router_queue_size)
+    object_L.append(router_c)
+    router_d = network_3.Router(name='D', intf_count=1, max_queue_size=router_queue_size)
+    object_L.append(router_d)
+
+    # create a Link Layer to keep track of links between network nodes
+    link_layer = link_3.LinkLayer()
     object_L.append(link_layer)
-    
-    #add all the links
-    #link parameters: from_node, from_intf_num, to_node, to_intf_num, mtu
-    link_layer.add_link(link_1.Link(client, 0, router_a, 0, 50))
-    link_layer.add_link(link_1.Link(router_a, 0, server, 0, 50))
-    
-    
-    #start all the objects
+
+    # add all the links
+    # link parameters: from_node, from_intf_num, to_node, to_intf_num, mtu
+    link_layer.add_link(link_3.Link(client_1, 0, router_a, 0, 50))
+    link_layer.add_link(link_3.Link(client_2, 0, router_a, 0, 50))
+    link_layer.add_link(link_3.Link(router_a, 0, router_b, 0, 50))
+    link_layer.add_link(link_3.Link(router_a, 0, router_c, 0, 50))
+    link_layer.add_link(link_3.Link(router_b, 0, router_d, 0, 50))
+    link_layer.add_link(link_3.Link(router_c, 0, router_d, 0, 50))
+    link_layer.add_link(link_3.Link(router_c, 0, router_d, 0, 50))
+    link_layer.add_link(link_3.Link(router_c, 0, router_d, 0, 50))
+    link_layer.add_link(link_3.Link(router_d, 0, server_1, 0, 50))
+    link_layer.add_link(link_3.Link(router_d, 0, server_2, 0, 50))
+
+    # start all the objects
     thread_L = []
-    thread_L.append(threading.Thread(name=client.__str__(), target=client.run))
-    thread_L.append(threading.Thread(name=server.__str__(), target=server.run))
+    thread_L.append(threading.Thread(name=client_1.__str__(), target=client_1.run))
+    thread_L.append(threading.Thread(name=client_2.__str__(), target=client_2.run))
+    thread_L.append(threading.Thread(name=server_1.__str__(), target=server_1.run))
+    thread_L.append(threading.Thread(name=server_2.__str__(), target=server_2.run))
     thread_L.append(threading.Thread(name=router_a.__str__(), target=router_a.run))
-    
+    thread_L.append(threading.Thread(name=router_a.__str__(), target=router_b.run))
+    thread_L.append(threading.Thread(name=router_a.__str__(), target=router_c.run))
+    thread_L.append(threading.Thread(name=router_a.__str__(), target=router_d.run))
+
     thread_L.append(threading.Thread(name="Network", target=link_layer.run))
-    
+
     for t in thread_L:
         t.start()
-    
-    
-    #create some send events    
+
+    # create some send events
     for i in range(3):
-        client.udt_send(2, 'Sample data %d and this is also my test string that should be atleast 80 characters long' % i)
-    
-    
-    #give the network sufficient time to transfer all packets before quitting
+        client_1.udt_send(2, 'Sample data %d and this is also my test string that should be atleast 80 characters long' % i)
+        client_2.udt_send(2, 'Sample data %d and this is also my test string that should be atleast 80 characters long' % i)
+
+    # give the network sufficient time to transfer all packets before quitting
     sleep(simulation_time)
-    
-    #join all threads
+
+    # join all threads
     for o in object_L:
         o.stop = True
     for t in thread_L:
         t.join()
-        
+
     print("All simulation threads joined")
-
-
 
 # writes to host periodically
