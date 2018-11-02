2679534d64e638a0eccf5654a6c4c595845c97ec
Dillon Tice <dtice@localhost.localdomain>
2018-11-02 15:32:29 -0600

    part 2 is done

--- network_2.py
+++ network_2.py
@@ -7,160 +7,179 @@ import queue
 import threading
 import random
 
-## wrapper class for a queue of packets
+# wrapper class for a queue of packets
+
+
 class Interface:
-    ## @param maxsize - the maximum size of the queue storing packets
+    # @param maxsize - the maximum size of the queue storing packets
     def __init__(self, maxsize=0):
-        self.queue = queue.Queue(maxsize);
+        self.queue = queue.Queue(maxsize)
         self.mtu = None
-    
-    ##get packet from the queue interface
+
+    # get packet from the queue interface
     def get(self):
         try:
             return self.queue.get(False)
         except queue.Empty:
             return None
-        
-    ##put the packet into the interface queue
+
+    # put the packet into the interface queue
     # @param pkt - Packet to be inserted into the queue
     # @param block - if True, block until room in queue, if False may throw queue.Full exception
     def put(self, pkt, block=False):
         self.queue.put(pkt, block)
-        
-        
-## Implements a network layer packet (different from the RDT packet 
+
+
+# Implements a network layer packet (different from the RDT packet
 # from programming assignment 2).
 # NOTE: This class will need to be extended to for the packet to include
 # the fields necessary for the completion of this assignment.
 class NetworkPacket:
-    ## packet encoding lengths 
+    # packet encoding lengths
     dst_addr_S_length = 5
-    
-    ##@param dst_addr: address of the destination host
+
+    # @param dst_addr: address of the destination host
     # @param data_S: packet payload
     def __init__(self, dst_addr, data_S):
         self.dst_addr = dst_addr
         self.data_S = data_S
-        
-    ## called when printing the object
+
+    # called when printing the object
     def __str__(self):
         return self.to_byte_S()
-        
-    ## convert packet to a byte string for transmission over links
+
+    # convert packet to a byte string for transmission over links
     def to_byte_S(self):
         byte_S = str(self.dst_addr).zfill(self.dst_addr_S_length)
         byte_S += self.data_S
         return byte_S
-    
-    ## extract a packet object from a byte string
+
+    # extract a packet object from a byte string
     # @param byte_S: byte string representation of the packet
     @classmethod
     def from_byte_S(self, byte_S):
-        dst_addr = int(byte_S[0 : NetworkPacket.dst_addr_S_length])
-        data_S = byte_S[NetworkPacket.dst_addr_S_length : ]
+        dst_addr = int(byte_S[0:NetworkPacket.dst_addr_S_length])
+        data_S = byte_S[NetworkPacket.dst_addr_S_length:]
         return self(dst_addr, data_S)
-    
 
-    
 
-## Implements a network host for receiving and transmitting data
+# Implements a network host for receiving and transmitting data
 class Host:
-    
-    ##@param addr: address of this node represented as an integer
+
+    # @param addr: address of this node represented as an integer
     def __init__(self, addr):
         self.addr = addr
         self.in_intf_L = [Interface()]
         self.out_intf_L = [Interface()]
-        self.stop = False #for thread termination
-    
-    ## called when printing the object
+        self.stop = False  # for thread termination
+        self.msgBuffer = []
+        self.currentID = 0
+    # called when printing the object
     def __str__(self):
         return 'Host_%s' % (self.addr)
-       
-    ## create a packet and enqueue for transmission
+
+    # create a packet and enqueue for transmission
     # @param dst_addr: destination address for the packet
     # @param data_S: data being transmitted to the network layer
     def udt_send(self, dst_addr, data_S):
-        if len(data_S) > (23):
+        # if the message is more than 19 char
+        if len(data_S) > (19):
             len_remain = len(data_S)
             fragflag = 1
-            id = random.randint(1,10)
+            offset = 0
+            id = random.randint(1, 9)
             while len_remain > 0:
                 data_split = ''
-                for i in range(23):
-                    if len(data_S) < 1:
-                        fragflag = 0
-                        break
-                    data_split = data_split + data_S[0]
-                    data_S = data_S[1:]
-                data_split = str(id) + str(fragflag) + data_split
+                # for each character
+                if len_remain < 19:
+                    fragflag = 0
+                    for i in range(len_remain):
+                        data_split = data_split + data_S[0]
+                        data_S = data_S[1:]
+                else:
+                    for i in range(19):
+                        data_split = data_split + data_S[0]
+                        data_S = data_S[1:]
+                offset += 19
                 len_remain = len_remain - len(data_split)
+                data_split = str(id) + str(fragflag) + str(offset) + data_split
                 p = NetworkPacket(dst_addr, data_split)
-                self.out_intf_L[0].put(p.to_byte_S()) #send packets always enqueued successfully
+                self.out_intf_L[0].put(p.to_byte_S())  # send packets always enqueued successfully
                 print('%s: sending packet "%s" on the out interface with mtu=%d' % (self, p, self.out_intf_L[0].mtu))
+            offset = 0
             return
+        # packet doesn't need to be split
         else:
             p = NetworkPacket(dst_addr, data_S)
-            self.out_intf_L[0].put(p.to_byte_S()) #send packets always enqueued successfully
+            self.out_intf_L[0].put(p.to_byte_S())  # send packets always enqueued successfully
+            print("reached else")
             print('%s: sending packet "%s" on the out interface with mtu=%d' % (self, p, self.out_intf_L[0].mtu))
             return
 
-    ## receive packet from the network layer
+    # receive packet from the network layer
     def udt_receive(self):
         pkt_S = self.in_intf_L[0].get()
         if pkt_S is not None:
             destination = str(pkt_S[0:5])
             id = int(pkt_S[5])
             fragflag = int(pkt_S[6])
-            offset = pkt_S[7:10]
+            offset = pkt_S[7:9]
             offset = int(offset)
-            self.dict['id'] = id
-            self.dict['offset'] = offset
-            self.dict['fragflag'] = fragflag 
+            msgTuple = (destination, id, fragflag, offset, pkt_S[9:])
+            self.msgBuffer.append(msgTuple)
             print('%s: received packet "%s" on the in interface' % (self, pkt_S))
-       
-    ## thread target for the host to keep receiving data
+            if fragflag == 0:
+                sorted(self.msgBuffer, key=lambda packet: packet[3])
+                something = ""
+                for msg in self.msgBuffer:
+                    something += msg[4]
+                print('\n')
+                print(something)
+                print('\n')
+                self.msgBuffer = []
+
+    # thread target for the host to keep receiving data
     def run(self):
         print (threading.currentThread().getName() + ': Starting')
         while True:
-            #receive data arriving to the in interface
+            # receive data arriving to the in interface
             self.udt_receive()
-            #terminate
+            # terminate
             if(self.stop):
                 print (threading.currentThread().getName() + ': Ending')
                 return
-        
+
+# Implements a multi-interface router described in class
 
 
-## Implements a multi-interface router described in class
 class Router:
-    
-    ##@param name: friendly router name for debugging
-    # @param intf_count: the number of input and output interfaces 
+
+    # @param name: friendly router name for debugging
+    # @param intf_count: the number of input and output interfaces
     # @param max_queue_size: max queue length (passed to Interface)
     def __init__(self, name, intf_count, max_queue_size):
-        self.stop = False #for thread termination
+        self.stop = False  # for thread termination
         self.name = name
-        #create a list of interfaces
+        # create a list of interfaces
         self.in_intf_L = [Interface(max_queue_size) for _ in range(intf_count)]
         self.out_intf_L = [Interface(max_queue_size) for _ in range(intf_count)]
 
-    ## called when printing the object
+    # called when printing the object
     def __str__(self):
         return 'Router_%s' % (self.name)
 
-    ## look through the content of incoming interfaces and forward to
+    # look through the content of incoming interfaces and forward to
     # appropriate outgoing interfaces
     def forward(self):
         for i in range(len(self.in_intf_L)):
             pkt_S = None
             try:
-                #get packet from interface i
+                # get packet from interface i
                 pkt_S = self.in_intf_L[i].get()
-                #if packet exists make a forwarding decision
+                # if packet exists make a forwarding decision
                 if pkt_S is not None:
                     p = NetworkPacket.from_byte_S(pkt_S) #parse a packet out
-                    # HERE you will need to implement a lookup into the 
+                    # HERE you will need to implement a lookup into the
                     # forwarding table to find the appropriate outgoing interface
                     # for now we assume the outgoing interface is also i
                     self.out_intf_L[i].put(p.to_byte_S(), True)
@@ -169,12 +188,12 @@ class Router:
             except queue.Full:
                 print('%s: packet "%s" lost on interface %d' % (self, p, i))
                 pass
-                
-    ## thread target for the host to keep forwarding data
+
+    # thread target for the host to keep forwarding data
     def run(self):
         print (threading.currentThread().getName() + ': Starting')
         while True:
             self.forward()
             if self.stop:
                 print (threading.currentThread().getName() + ': Ending')
-                return 
+                return
